5869a548a3ac0bb2acc439ae706ba22bc8932f58
EM-H20 <ghd0106@gmail.com>
2024-12-24 14:17:09 +0900

    LostArk API사용하지 않아서 삭제

--- data.py
+++ data.py
@@ -5,5 +5,4 @@ load_dotenv()
 
 Token = os.getenv('DISCORD_TOKEN')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-LostArk_API_KEY = os.getenv('LostArk_API_KEY')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
